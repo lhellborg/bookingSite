0f2d4dbbd1eae00b138e50615670694ede6fe4db
Linda Hellborg <linda.hellborg@gmail.com>
2016-08-23 15:17:45 +0200

    add filtering of items

--- app/scripts/controllers/skins.js
+++ app/scripts/controllers/skins.js
@@ -16,26 +16,26 @@ angular.module('lambSkinsApp')
 
   	// vm.products = $firebaseObject(myFirebaseRef);
   	vm.products = $firebaseArray(myFirebaseRef);
-  	console.log(vm.products)
+  	console.log(vm.products);
 
 // used the first time to set some data in firebase
 
 	// myFirebaseRef.set({
-	// 	1 : {'productName':'lamb1','nr':'1','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	2 : {'productName':'lamb2','nr':'2','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	3 : {'productName':'lamb3','nr':'3','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	4 : {'productName':'lamb4','nr':'4','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	5 : {'productName':'lamb3','nr':'5','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	6 : {'productName':'lamb6','nr':'6','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	7 : {'productName':'lamb7','nr':'7','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	8 : {'productName':'lamb8','nr':'8','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	9 : {'productName':'lamb9','nr':'9','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	10 : {'productName':'lamb10','nr':'10','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	11 : {'productName':'lamb11','nr':'11','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	12 : {'productName':'lamb12','nr':'12','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	13 : {'productName':'lamb13','nr':'13','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	14 : {'productName':'lamb14','nr':'14','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
-	// 	15 : {'productName':'lamb15','nr':'15','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	1 : {'productName':'lamb1','nr':'1','color':'dark','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	2 : {'productName':'lamb2','nr':'2','color':'dark','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	3 : {'productName':'lamb3','nr':'3','color':'dark','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	4 : {'productName':'lamb4','nr':'4','color':'dark','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	5 : {'productName':'lamb3','nr':'5','color':'dark','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	6 : {'productName':'lamb6','nr':'6','color':'medium','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	7 : {'productName':'lamb7','nr':'7','color':'medium','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	8 : {'productName':'lamb8','nr':'8','color':'medium','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	9 : {'productName':'lamb9','nr':'9','color':'white','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	10 : {'productName':'lamb10','nr':'10','color':'white','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	11 : {'productName':'lamb11','nr':'11','color':'white','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	12 : {'productName':'lamb12','nr':'12','color':'white','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	13 : {'productName':'lamb13','nr':'13','color':'white','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	14 : {'productName':'lamb14','nr':'14','color':'white','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
+	// 	15 : {'productName':'lamb15','nr':'15','color':'white','reservedByName':"","reservedByTel":"","reservedByEmail":"","sold":false},
 	// });
 
 
@@ -60,8 +60,16 @@ angular.module('lambSkinsApp')
 	vm.firstTabStop;
 	vm.lastTabStop;
 
+	vm.filterItem = function() {
+	     // deferred this because the dom element might not be there yet
+	    setTimeout(function() {
+	        $(".bokBtn:first").focus();
+	    }, 1);
+	};
 
-	vm.modalToggle = function(details, $event) {
+
+
+	vm.modalToggle = function(details) {
 		$("#reserveItems").attr("aria-hidden", "true"); //unable to interact with the items on the main page
 		vm.focusedElementBeforeModal = document.activeElement; //save the current focused element to get back to when closing the modal
 		vm.selectedProduct = details;
@@ -82,7 +90,7 @@ angular.module('lambSkinsApp')
 			if(e.shiftKey) { //shift TAB key press
 				if(document.activeElement === vm.firstTabStop) {
 					e.preventDefault();
-					vm.lastTabStop.focus()
+					vm.lastTabStop.focus();
 				}
 			} else { //TAB
 				if(document.activeElement === vm.lastTabStop) {
@@ -94,18 +102,18 @@ angular.module('lambSkinsApp')
 		if(e.keyCode === 27) { //Esc key
 			vm.closeModal();
 		}
-	};
+	}
 
 	vm.closeModal = function() {
 		$("#reserveModal").modal('hide');
 		$("#reserveModal").attr("aria-hidden", "true"); //unable voiceover interaction with the modal window
 		$("#reserveItems").attr("aria-hidden", "false"); //enable voiceover interaction with the items on the main page
 		vm.focusedElementBeforeModal.focus();
-	}
+	};
 
 	vm.submitForm = function(selectedProduct) {
 
-		var valid = validateInput(vm.reservationForm.name, vm.reservationForm.email) //check all the required fields
+		var valid = validateInput(vm.reservationForm.name, vm.reservationForm.email); //check all the required fields
 
 		if (valid) {
 			if (vm.selectedProduct.sold) {
@@ -122,8 +130,8 @@ angular.module('lambSkinsApp')
 					$(".closeBtn").focus();
 					vm.focusedElementBeforeModal = $(".infoBox"); //get focus back to the side after reserved a product
 				}, function(error) {
-					console.log("The read failed: " + errorObject.code);
-					alert("Sorry, could not get the bookings now, try again later :-)")
+					console.log("The read failed: " + error.code);
+					alert("Sorry, could not get the bookings now, try again later :-)");
 				});
 
 			}
@@ -131,21 +139,11 @@ angular.module('lambSkinsApp')
 			vm.messageToBuyer = "Please fill in all fields";
 		}
 
-	}
-
-
-
-	// 	}, function (errorObject) {
-	// 	  console.log("The read failed: " + errorObject.code);
-	// 	  alert("Sorry could not get the bookings, try again later :-)");
-	// 	});
-	// };
-
+	};
 
 
 		// Since a required field that has never been "touched" will not show any error message, each input that is required has a class "accountRequired" that can be checked to see that the value is not empty.
 	function validateInput(name, email) {
-
 		return name && email;
 	}
 
